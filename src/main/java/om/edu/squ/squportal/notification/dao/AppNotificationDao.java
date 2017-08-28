d72637cc8cc2223c2b1a7794906f2be1dbbf8697
rayees80 <rayees80@squ.edu.om>
2017-08-28 13:49:17 +0400

    added method to get notification list based on project

--- src/main/java/om/edu/squ/squportal/notification/dao/AppNotificationDao.java
+++ src/main/java/om/edu/squ/squportal/notification/dao/AppNotificationDao.java
@@ -1,8 +1,14 @@
 package om.edu.squ.squportal.notification.dao;
 
+import java.util.List;
+
 import om.edu.squ.squportal.notification.dto.AppNotificationDto;
 
 public interface AppNotificationDao
 {
 	public long insert(AppNotificationDto dto);
+
+	public List<AppNotificationDto> getAll(String appName);
+
+
 }
